d70eb6378bfbae86df625936a30aefecbf228874
Mayan Atid <mayanatid@gmail.com>
2017-05-03 14:45:55 -0700

    Forms

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(Form):
+	first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
+	last_name = StringField("Last name", validators=[DataRequired("Please enter your last name")])
+	email_unique = StringField('Email', validators=[DataRequired("Please enter your email"), Email("Please use correct email address format")])
+	password =PasswordField('Password', validators=[DataRequired("Please enter your password"), Length(min=6, message="Passwords must be longer than 6 characters")])
+	submit = SubmitField('Sign up')
